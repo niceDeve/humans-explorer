6b95a058249000bd5ee73a10925b48a450efb4fe
niceDeve <nicedeve785@gmail.com>
2022-09-23 18:06:05 +0300

    fix: redesigned footer

--- src/@core/layouts/components/AppFooter.vue
+++ src/@core/layouts/components/AppFooter.vue
@@ -1,84 +1,77 @@
 <template>
   <b-container class="pb-1 mt-2">
-    <b-row class="align-items-center justify-content-center px-1">
-      <b-col
-        cols="14"
-      >
-        <div class="d-flex justify-content-between">
-          <div class="d-none d-md-block d-md-inline-block">
-            <b-link href="/devnet">
-              <b-img
-                src="/logos/humans.svg"
-                alt="humans logo"
-                width=""
-              />
-            </b-link>
-          </div>
-          <div class="d-flex flex-column justify-content-center align-items-center">
-            <div class="d-flex">
-              <b-link
-                class="mr-50"
-                href="/devnet"
-                target="_blank"
-              >Summary</b-link>
-              <b-link
-                class="ml-50 mr-50"
-                href="/devnet/blocks"
-                target="_blank"
-              >Blocks</b-link>
-              <b-link
-                class="ml-50 mr-50"
-                href="/devnet/staking"
-                target="_blank"
-              >Staking</b-link>
-              <b-link
-                class="ml-50"
-                href="/devnet/governance"
-                target="_blank"
-              >Governance</b-link>
-              <b-link
-                class="ml-50"
-                href="/devnet/uptime"
-                target="_blank"
-              >Uptime</b-link>
-            </div>
-            <div class="d-flex">
-              <b-link
-                class="ml-50 mr-50"
-                href="http://github.com/humansdotai"
-                target="_blank"
-              ><feather-icon
-                icon="GithubIcon"
-                size="16"
-              /> Github</b-link>
-              <b-link
-                class="ml-50 mr-50"
-                href="https://discord.gg/humansdotai"
-                target="_blank"
-              ><feather-icon
-                icon="EyeIcon"
-                size="16"
-              /> Discord</b-link>
-              <b-link
-                class="ml-50 mr-50"
-                href="https://twitter.com/humansdotai"
-                target="_blank"
-              ><feather-icon
-                icon="TwitterIcon"
-                size="16"
-              /> Twitter</b-link>
-
-            </div>
-          </div>
+    <div class="d-flex justify-content-between">
+      <div class="d-none d-md-block d-md-inline-block">
+        <b-link href="/devnet">
+          <b-img
+            src="/logos/humans.svg"
+            alt="humans logo"
+            width=""
+          />
+        </b-link>
+      </div>
+      <div class="d-flex justify-content-between justify-content-md-end w-100">
+        <div class="d-flex flex-column mr-4">
+          <b-link
+            class="mr-1 mb-1"
+            href="/devnet"
+            target="_blank"
+          >Summary</b-link>
+          <b-link
+            class="mr-1 mb-1"
+            href="/devnet/blocks"
+            target="_blank"
+          >Blocks</b-link>
+          <b-link
+            class="mr-1 mb-1"
+            href="/devnet/staking"
+            target="_blank"
+          >Staking</b-link>
+          <b-link
+            class="mr-1 mb-1"
+            href="/devnet/governance"
+            target="_blank"
+          >Governance</b-link>
+          <b-link
+            class=""
+            href="/devnet/uptime"
+            target="_blank"
+          >Uptime</b-link>
         </div>
-      </b-col>
-    </b-row>
+        <div class="d-flex flex-column">
+          <b-link
+            class="mb-1"
+            href="http://github.com/humansdotai"
+            target="_blank"
+          ><feather-icon
+            icon="GithubIcon"
+            size="16"
+          /> Github</b-link>
+          <b-link
+            class="mb-1"
+            href="https://discord.gg/humansdotai"
+            target="_blank"
+          ><feather-icon
+            icon="EyeIcon"
+            size="16"
+          /> Discord</b-link>
+          <b-link
+            class=""
+            href="https://twitter.com/humansdotai"
+            target="_blank"
+          ><feather-icon
+            icon="TwitterIcon"
+            size="16"
+          /> Twitter</b-link>
+        </div>
+      </div>
+    </div>
   </b-container>
 </template>
 
 <script>
 import {
-  BLink, VBPopover, BContainer, BRow, BCol, BImg,
+  BLink, VBPopover, BContainer, BImg,
 } from 'bootstrap-vue'
 import Ripple from 'vue-ripple-directive'
 
@@ -87,8 +80,6 @@ export default {
   components: {
     BLink,
     BContainer,
-    BRow,
-    BCol,
     BImg,
   },
   directives: {
